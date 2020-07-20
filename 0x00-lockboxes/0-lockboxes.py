8ae6fec6f3ac3b9ebbc6900b30c2bd3e579121e1
AngelPedroza <angelomarpedrozacardenas@gmail.com>
2020-07-20 10:20:55 -0500

    [Change] Change recursion to looping

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -1,34 +1,20 @@
 #!/usr/bin/python3
-def function(index, boxes, keys_dict):
-    for i in boxes[index]:
-        if i in list(keys_dict.keys()):
-            continue
-        keys_dict.update({i: None})
-        function(i, boxes, keys_dict)
+
 
 def canUnlockAll(boxes):
     """Know if can entry to all sublists"""
     if len(boxes) == 0:
         return False
 
-    keys_dict = {}
-    keys_dict[0] = None
-    for keys in boxes[0]:
-        keys_dict[keys] = None
+    iBox = [0]
 
-    for index in boxes[0]:
-        if index >= len(boxes):
-            continue
-        else:
-            function(index, boxes, keys_dict)
+    for key in iBox:
+        for keyBox in boxes[key]:
+            if keyBox not in iBox:
+                if keyBox < len(boxes):
+                    iBox.append(keyBox)
 
-    list_boxes = []
-    for index in list(keys_dict.keys()):
-        if index >= len(boxes):
-            continue
-        else:
-            list_boxes.append(boxes[index])
-
-    if len(list_boxes) == len(boxes):
+    if len(iBox) == len(boxes):
         return True
+
     return False
