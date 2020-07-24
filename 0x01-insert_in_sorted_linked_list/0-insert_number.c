de62b9b854ebde07e8dfeaca252b34c9db73d1af
AngelPedroza <angelomarpedrozacardenas@gmail.com>
2020-07-23 21:34:18 -0500

    [Fix] Betty Style

--- 0x01-insert_in_sorted_linked_list/0-insert_number.c
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -7,27 +7,28 @@
  * @number: The value to add in the new node
  * Return: the address of the new node, or NULL if it failed
  **/
- listint_t *insert_node(listint_t **head, int number)
- {   
-    listint_t *tmp = *head;
-    listint_t *new;
 
-    new = malloc(sizeof(listint_t));
-    if (new == NULL)
-        return (NULL);
-    new->n = number;
-    new->next = NULL; 
+listint_t *insert_node(listint_t **head, int number)
+{
+        listint_t *tmp = *head;
+        listint_t *new;
 
-    while (tmp->next)
-    {
-        if (number >= tmp->n && number < tmp->next->n)
+        new = malloc(sizeof(listint_t));
+        if (new == NULL)
+                return (NULL);
+        new->n = number;
+        new->next = NULL;
+
+        while (tmp->next)
         {
-            new->next = tmp->next;
-            tmp->next = new;
-            return (new);
+                if (number >= tmp->n && number < tmp->next->n)
+                {
+                        new->next = tmp->next;
+                        tmp->next = new;
+                        return (new);
+                }
+                tmp = tmp->next;
         }
-        tmp = tmp->next;
-    }
 
-    return (NULL);
- }
+        return (NULL);
+}
