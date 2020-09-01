6c2c2c127a17a7b08f70ff718df4ee4f0ad78c94
AngelPedroza <angelomarpedrozacardenas@gmail.com>
2020-08-31 21:37:45 -0500

    [Fix] Pep8 style

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
+
 import sys
+
 
 def print_status(status_dict, file_size):
     """Print the format"""
@@ -9,16 +11,8 @@ def print_status(status_dict, file_size):
         if status_dict[key] > 0:
             print("{}: {}".format(key, status_dict[key]))
 
-status_dict = {
-    '200': 0, 
-    '301': 0,
-    '400': 0,
-    '401': 0,
-    '403': 0,
-    '404': 0, 
-    '405': 0,
-    '500': 0
-    }
+status_dict = {'200': 0, '301': 0, '400': 0, '401': 0, '403': 0,
+               '404': 0, '405': 0, '500': 0}
 
 file_size = 0
 count = 0
@@ -30,13 +24,13 @@ try:
 
         el = line.split(" ")
         count += 1
-        
+
         try:
             if el[7] in status_dict.keys():
                 status_dict[el[7]] += 1
         except:
             pass
-        
+
         try:
             file_size += int(el[8])
         except:
