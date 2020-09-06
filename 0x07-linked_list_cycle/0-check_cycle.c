a5e99263dfa757a218e684919d80ad305093443c
AngelPedroza <angelomarpedrozacardenas@gmail.com>
2020-09-06 13:30:30 -0500

    [Add] Use to pointers, if the pointer make match, the list is a circle

---
+++ 0x07-linked_list_cycle/0-check_cycle.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+int check_cycle(listint_t *list)
+{
+	listint_t *bunny = list, *turtle = list;
+
+	while (turtle->next && bunny->next)
+	{
+		turtle = turtle->next;
+
+		if (bunny->next->next)
+			bunny = bunny->next->next;
+		else
+			break;
+
+		if (bunny->n == turtle->n)
+			return (1);
+	}
+	return (0);
+}
