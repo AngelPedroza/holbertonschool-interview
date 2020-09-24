4a7b4be9272cdb78f06e156d3acc6bad081f79c8
AngelPedroza <angelomarpedrozacardenas@gmail.com>
2020-09-23 22:15:54 -0500

    [Fix] Fix pep8 style

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -14,26 +14,18 @@ def validUTF8(data):
 
     for i in data:
         mask_n_byte = 1 << 7
-        
         if countBytes == 0:
-            
             while mask_n_byte & i:
                 countBytes += 1
                 mask_n_byte = mask_n_byte >> 1
-
             if countBytes == 0:
                 continue
-            
             if countBytes == 1 or countBytes > 4:
                 return False
         else:
-            
             if not (i & oneVer and not (i & secondVer)):
                 return False
-        
         countBytes -= 1
-    
     if countBytes == 0:
         return True
-    
     return False
