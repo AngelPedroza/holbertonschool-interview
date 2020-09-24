e9e6ed50b5cc7cfda2e7f86082b74262e29d4cbf
AngelPedroza <angelomarpedrozacardenas@gmail.com>
2020-09-23 22:14:00 -0500

    [Use] An internet example

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -4,11 +4,36 @@
 
 def validUTF8(data):
     """
-    Say if an number is a valid UTF-8
+    Return: True if data is a valid UTF-8 encoding, else return False
+    A character in UTF-8 can be 1 to 4 bytes long
+    The data set can contain multiple characters
     """
-    for i in data:
-        bin = '{0:08b}'.format(i)
-        if len(bin) > 8:
-            return False
+    countBytes = 0
+    oneVer = 1 << 7
+    secondVer = 1 << 6
 
-    return True
+    for i in data:
+        mask_n_byte = 1 << 7
+        
+        if countBytes == 0:
+            
+            while mask_n_byte & i:
+                countBytes += 1
+                mask_n_byte = mask_n_byte >> 1
+
+            if countBytes == 0:
+                continue
+            
+            if countBytes == 1 or countBytes > 4:
+                return False
+        else:
+            
+            if not (i & oneVer and not (i & secondVer)):
+                return False
+        
+        countBytes -= 1
+    
+    if countBytes == 0:
+        return True
+    
+    return False
