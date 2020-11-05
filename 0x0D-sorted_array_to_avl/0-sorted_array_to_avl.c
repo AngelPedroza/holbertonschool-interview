ca9ad2982ddc35d41e2b0d353cc95ba1c96eec46
Angel Pedroza <angel.pedroza@rappi.com>
2020-11-04 20:58:06 -0500

    [add] Sort AVL Tree

--- 0x0D-sorted_array_to_avl/0-sorted_array_to_avl.c
+++ 0x0D-sorted_array_to_avl/0-sorted_array_to_avl.c
@@ -4,7 +4,6 @@
  * binary_tree_node - creates a binary tree node
  * @parent: pointer to the parent node of the node to create
  * @value: value to put in the new node
- *
  * Return: New Node or NULL if fail
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
