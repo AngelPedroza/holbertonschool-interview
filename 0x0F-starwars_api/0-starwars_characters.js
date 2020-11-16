0ac9635b62e086065771b86a50e519a8119142a5
Angel Pedroza <angel.pedroza@rappi.com>
2020-11-16 13:05:27 -0500

    [Fix] Semistandar style

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -1,17 +1,18 @@
 #!/usr/bin/node
-request = require('request')
-request({method: 'GET', uri: `https://swapi-api.hbtn.io/api/films/${process.argv[2]}`}, (error, response, body) => {
-    if (!error && response.statusCode == 200) {
-        const res = JSON.parse(body)
-        const characters = res.characters
-        for (let i in characters){
-            request({method: 'GET', uri: characters[i]}, (error, response, body) => {
-                if (!error && response.statusCode == 200) {
-                    const res_char = JSON.parse(body)
-                    const name = res_char.name
-                    console.log(name)
-                }
-            })
+const request = require('request');
+
+request({ method: 'GET', uri: `https://swapi-api.hbtn.io/api/films/${process.argv[2]}` }, (error, response, body) => {
+  if (!error && response.statusCode === 200) {
+    const res = JSON.parse(body);
+    const characters = res.characters;
+    for (const i in characters) {
+      request({ method: 'GET', uri: characters[i] }, (error, response, body) => {
+        if (!error && response.statusCode === 200) {
+          const resChar = JSON.parse(body);
+          const name = resChar.name;
+          console.log(name);
         }
+      });
     }
-})
+  }
+});
