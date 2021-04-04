a062c5b69435168a8e9a533b3a109851fce66a2c
Angel Pedroza <angel.pedroza@rappi.com>
2021-04-04 11:30:03 -0500

    [ADD]test

--- 0x1F-pascal_triangle/0-pascal_triangle.py
+++ 0x1F-pascal_triangle/0-pascal_triangle.py
@@ -2,47 +2,40 @@
 """
 0-pascal_triangle
 """
-
-
-def factorial(n):
-    """Generate the factorial of a number"""
-    fact = 1
-
-    for i in range(1, n+1):
-        fact = fact * i
-
-    return fact
-
-
-def combination(n, r):
-    """Get the value by the position in the triangle"""
-    a = factorial(n) / (factorial(r) * factorial(n - r))
-    return int(a)
-
-
-def __pascal_triangle(n):
-    """Create the pascal triangle"""
-    if n <= 0:
-        return [[]]
-
-    result = []
-    for count in range(n):
-        row = []
-        for element in range(count + 1):
-            row.append(combination(count, element))
-        result.append(row)
-
-    return result
+#
+# def factorial(n):
+#     """Generate the factorial of a number"""
+#     fact = 1
+#
+#     for i in range(1, n+1):
+#         fact = fact * i
+#
+#     return fact
+#
+#
+# def combination(n, r):
+#     """Get the value by the position in the triangle"""
+#     a = factorial(n) / (factorial(r) * factorial(n - r))
+#     return int(a)
+# def pascal_triangle(n):
+#     """Create the pascal triangle"""
+#     if n <= 0:
+#         return []
+#
+#     result = []
+#     for count in range(n):
+#         row = []
+#         for element in range(count + 1):
+#             row.append(combination(count, element))
+#         result.append(row)
+#
+#     return result
 
 
 def pascal_triangle(n):
-    myList = [[1]]
-    if (n <= 0):
-        return [[]]
-    for i in range(1, n):
-        myList.append([1])
-        for b in range(1, i):
-            tmp = int(myList[i - 1][b]) + int(myList[i - 1][b - 1])
-            myList[i].append(str(tmp))
-        myList[i].append(1)
-    return myList
+    """Computes N rows of Pascal's Triangle"""
+    pascal = [[1]]
+    for _ in range(n - 1):
+        pascal += [[1] + [pascal[-1][i] + pascal[-1][i + 1]
+                   for i in range(len(pascal[-1]) - 1)] + [1]]
+    return pascal if n > 0 else []
