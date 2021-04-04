1fe11758d1b776cf92f867bf0e0faf720d80e7ff
Angel Pedroza <angel.pedroza@rappi.com>
2021-04-04 11:09:44 -0500

    [ADD] the 11** doesn't work after 5 element

--- 0x1F-pascal_triangle/0-pascal_triangle.py
+++ 0x1F-pascal_triangle/0-pascal_triangle.py
@@ -1,10 +1,32 @@
+#!/usr/bin/python3
+"""
+0-pascal_triangle
+"""
+
+
+def factorial(n):
+    """Generate the factorial of a number"""
+    fact = 1
+
+    for i in range(1, n+1):
+        fact = fact * i
+
+    return fact
+
+
+def combination(n, r):
+    """Get the value by the position in the triangle"""
+    a = factorial(n) / (factorial(r) * factorial(n - r))
+    return int(a)
+
+
 def pascal_triangle(n):
-    if n <= 0:
-        return []
+    """Create the pascal triangle"""
+    result = []
+    for count in range(n):
+        row = []
+        for element in range(count + 1):
+            row.append(combination(count, element))
+        result.append(row)
 
-    big = []
-    for number in range(n):
-        a = [int(i) for i in str(11**number)]
-        big.append(a)
-
-    return big
+    return result
